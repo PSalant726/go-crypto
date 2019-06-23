caa9a58d15e0e4ba2c8fba5bd412479e5c61c8a0
Phil Salant <PSalant@gmail.com>
2019-06-23 11:56:24 -0400

    Optimize the Merkle Tree

--- merkle_tree.go
+++ merkle_tree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"log"
 )
 
 type MerkleTree struct {
@@ -17,20 +18,23 @@ type MerkleNode struct {
 func NewMerkleTree(data [][]byte) *MerkleTree {
 	var nodes []MerkleNode
 
-	if len(data)%2 != 0 {
-		data = append(data, data[len(data)-1])
-	}
-
 	for _, datum := range data {
 		node := NewMerkleNode(nil, nil, datum)
 		nodes = append(nodes, *node)
 	}
 
-	for i := 0; i < len(data)/2; i++ {
-		var newLevel []MerkleNode
+	if len(nodes) == 0 {
+		log.Panic("No merkel nodes")
+	}
 
-		for j := 0; j < len(nodes); j += 2 {
-			node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+	for len(nodes) > 1 {
+		if len(nodes)%2 != 0 {
+			nodes = append(nodes, nodes[len(nodes)-1])
+		}
+
+		var newLevel []MerkleNode
+		for i := 0; i < len(nodes); i += 2 {
+			node := NewMerkleNode(&nodes[i], &nodes[i+1], nil)
 			newLevel = append(newLevel, *node)
 		}
 
